f2bd64bf282d5c7001c897e3c7b199607421f14b
Brian Adams <badams2527@gmail.com>
2015-02-09 16:07:29 -0500

    Get the CollectorStream to work as it should.

--- src/Lab3/CollectorStream.java
+++ src/Lab3/CollectorStream.java
@@ -6,28 +6,34 @@ import java.util.Queue;
 /**
  * Created by badams on 2/9/15.
  */
-public class CollectorStream {
+public class CollectorStream extends Stream {
     Queue<Producer> producers = new LinkedList<Producer>();
     TCFrame t = null;
+    Notifier n = null;
+    double value = 0;
+
+    public CollectorStream(){}
 
     public CollectorStream(Notifier n, TCFrame t) {
         // TODO: Add logic for this constructor
         this.t = t;
+        this.n = n;
     }
 
     public void add(Producer p){
         producers.add(p);
-        t.area.append("Collector: got " + p.ident + "\n");
-    }
-
-    public Object next(){
-        // TODO: Add logic for this function
-        return null;
     }
 
     public void start(){
-        // TODO: Add logic for this function
+        for (Producer p : producers){
+            t.area.append("Collector: got " + p.ident + "\n");
+            Subscriber s = (Subscriber)p.next();
+            value += s.stock_value;
+        }
     }
 
-
+    synchronized public Object next () {
+        n.putValue(new IntObject((int) value));
+        return n;
+    }
 }
