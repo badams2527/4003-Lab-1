186054f7b6db208a928e749d58be091cd1235dec
Brian Adams <badams2527@gmail.com>
2015-04-17 01:27:58 -0400

    Get it to stop displaying negative number.

--- haskell-projects/Lab10.hs
+++ haskell-projects/Lab10.hs
@@ -1,8 +1,8 @@
 higgingsType = ["The", "rain", "in", "spain", "falls", "mainly", "on", "the", "plain"] :: [[Char]]
 
 inp :: [([Char], Double, Double)]
-inp = [("A",4,12), ("B",2,1), ("C",2,2), ("D",1,1), ("E",10,4)]
-manip lst = [ (name, v/w, w) | (name, v, w) <- lst ]
+inp = [("A",4,12), ("B",2,1), ("C",2,2), ("D",1,1), ("E",10,4), ("F", 1, 20)]
+manip lst = [ (name, v/w, w) | (name, v, w) <- lst]
 
 sec (_, x, _) = x
 
@@ -31,9 +31,12 @@ srt [] = []
 srt (a:[]) = [a]
 srt x = mrg (srt (splite x)) (srt (splito x))
 
+displayList :: (Fractional t, Ord t) => [([Char], t, t)] -> [([Char], t, t)]
+displayList lst = [(name, v, u) | (name, v, u) <- drop 1 lst, v > 0]
+
 knapsack :: (Fractional t, Ord t) => [([Char], t, t)] -> t -> [([Char], t, t)]
-knapsack lst w = drop 1 ans
-  where
-    ans = ("Start", w, 0):[ (name, v-u, if (u < v-u) then u; else v)
+knapsack lst w = 
+  let ans = ("Start", w, 0):[ (name, (v-u), if (u < v-u) then u; else v)
                           | (name, r, u) <- srt (manip lst)
                           | (crss, v, p) <- ans]
+  in displayList (ans)
